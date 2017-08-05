5aea2f34e5fe05c630af2fa7b1ea2d63b61e96bc
datasigntist <vishwanathanraman@vishwanathans-MacBook-Air.local>
2017-08-05 11:06:32 +0530

    Added heroku config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/CuratedContent',{
+//mongoose.connect('mongodb://localhost:27017/CuratedContent',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/CuratedContent' ,{
   useMongoClient: true
 });
 
